573f5f2fc61d21b89c2648f2a088f3466d640289
April Roszkowski <roszk008@umn.edu>
2019-12-26 17:43:06 -0600

    raytracer.php: adds words

--- raytracer.php
+++ raytracer.php
@@ -33,6 +33,10 @@
             text-indent: 1.5em;
             padding: 20px;
         }
+        #words li {
+            text-indent: 0;
+            text-
+        }
     </style>
 </head>
 
@@ -40,14 +44,12 @@
     <?php include 'html/header.html' ?>
 
     <div id='page-title'>
-        <h1>ProjectView</h1>
+        <h1>Raytracing</h1>
     </div>
     
     <div id='column'>
         <div id='gallery'>
             <div class='gallery-thumbnail-container'>
-                <div><img class='thumbnail' src='resources/placeholder.png' alt='placeholder image' onclick='expandImage(this)'></div>
-                <div><img class='thumbnail' src='resources/stone.jpg' alt='placeholder image' onclick='expandImage(this)'></div>
                 <div><img class='thumbnail' src='resources/placeholder.png' alt='placeholder image' onclick='expandImage(this)'></div>
             </div>
             <div class='gallery-main'>
@@ -62,44 +64,54 @@
                 </p>
             </div>
             <div>
-                <p><b>Languages:</b> C++</p>
+                <p>
+                    <b>Language:</b> C++
+                </p>
             </div>
         </div>
         <div id='words'>
             <h2>Summary</h2>
+            <h3>Background</h3>
+            <p>
+                Raytracing is a method of rendering a virtual scene which is typically used for photorealistic styles. This is
+                because the technique attempts to simulate the paths that light rays take through the scene before entering 
+                one's eye (more info <a href='https://en.wikipedia.org/wiki/Ray_tracing_(graphics)'>here</a>). What this means,
+                practically speaking, is that we can render physically-accurate scenes at an increased computational cost.
+                However, there has been growing interest in raytracing as hardware manufacturers have begun developing graphics
+                cards which optimize basic raytracing computation. 
+            </p>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Velit dignissim sodales ut eu. Pretium quam vulputate dignissim suspendisse in est ante in. Egestas egestas fringilla 
-                phasellus faucibus. Eget felis eget nunc lobortis mattis aliquam faucibus purus in. Commodo quis imperdiet massa 
-                tincidunt. Arcu dictum varius duis at consectetur lorem donec. Scelerisque purus semper eget duis at. Mattis nunc sed 
-                blandit libero volutpat sed cras. Quisque sagittis purus sit amet volutpat consequat mauris nunc congue. Nisl purus in 
-                mollis nunc sed id. Amet cursus sit amet dictum sit amet justo donec enim. Arcu non odio euismod lacinia at quis risus sed 
-                vulputate. Amet aliquam id diam maecenas ultricies mi eget mauris.</p>
-            <p>Donec adipiscing tristique risus nec feugiat in fermentum. Elit duis tristique sollicitudin nibh. A condimentum vitae sapien 
-                pellentesque habitant morbi tristique senectus et. Amet nisl purus in mollis nunc sed id. Sed enim ut sem viverra aliquet 
-                eget sit amet. Tellus pellentesque eu tincidunt tortor aliquam nulla. Aenean et tortor at risus. Eu sem integer vitae justo. 
-                In egestas erat imperdiet sed euismod nisi porta. Posuere morbi leo urna molestie at. Tempus quam pellentesque nec nam 
-                aliquam. Phasellus egestas tellus rutrum tellus.</p>    
-            <p>Vel risus commodo viverra maecenas accumsan lacus. Convallis convallis tellus id interdum velit laoreet id. Enim nulla
-                aliquet porttitor lacus luctus accumsan tortor. Massa massa ultricies mi quis hendrerit dolor magna eget est. Scelerisque 
-                felis imperdiet proin fermentum leo vel orci. Tortor pretium viverra suspendisse potenti nullam ac tortor vitae. Turpis 
-                egestas integer eget aliquet. Ac tortor dignissim convallis aenean et tortor. Fringilla phasellus faucibus scelerisque 
-                eleifend donec pretium vulputate sapien nec. Vehicula ipsum a arcu cursus vitae. Amet justo donec enim diam vulputate. 
-                Fusce id velit ut tortor pretium viverra suspendisse. Vitae auctor eu augue ut lectus arcu bibendum at. Rhoncus est 
-                pellentesque elit ullamcorper dignissim cras tincidunt. Dolor magna eget est lorem ipsum dolor sit amet. Scelerisque 
-                viverra mauris in aliquam sem fringilla ut. Amet purus gravida quis blandit turpis. Nisl tincidunt eget nullam non.</p>
-            <p>Ultricies mi quis hendrerit dolor magna eget. Ipsum a arcu cursus vitae congue mauris rhoncus aenean vel. Morbi tincidunt 
-                augue interdum velit. Massa massa ultricies mi quis. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Sed 
-                libero enim sed faucibus turpis in eu mi. Egestas erat imperdiet sed euismod nisi. Vel fringilla est ullamcorper eget nulla 
-                facilisi etiam dignissim. Mattis vulputate enim nulla aliquet. Diam ut venenatis tellus in metus. Maecenas pharetra 
-                convallis posuere morbi leo. Quisque non tellus orci ac auctor augue mauris augue. Eget nullam non nisi est sit amet. 
-                Vulputate dignissim suspendisse in est. Lacinia quis vel eros donec ac. Facilisis gravida neque convallis a cras semper.
-                Gravida neque convallis a cras semper auctor neque.</p>
-            <p>Lobortis mattis aliquam faucibus purus in. Consequat id porta nibh venenatis cras sed felis. Velit aliquet sagittis id 
-                consectetur purus ut faucibus. Eu scelerisque felis imperdiet proin fermentum leo vel orci. Dictum non consectetur a erat
-                nam at lectus urna. Vitae auctor eu augue ut lectus arcu bibendum at varius. Eget egestas purus viverra accumsan in nisl 
-                nisi. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Rhoncus dolor purus non enim praesent elementum
-                facilisis. Consequat interdum varius sit amet mattis vulputate. Proin nibh nisl condimentum id venenatis a condimentum.</p>
+            <h3>My work</h3>
+            <p>
+                Over half a semester, everyone in my class wrote their own raytracing program from essentially scratch.
+                Specifically, we wrote:
+                <ol>
+                    <li>
+                        An image writer. This was a relatively short function in C/C++ which writes a given array of colors to
+                        a file in ASCII .ppm format. Highly inefficient; ridiculously simple (compared to .jpg or .png).
+                    </li>
+                    <li>
+                        A raycaster. This is a few functions which shoots rays into a scene who return the color of whatever 
+                        object they hit.
+                    </li>
+                    <li>
+                        A scene description file reader. Some standard C file parsing used to read the objects (e.g. spheres or 
+                        .obj triangle meshes) in a scene, as well as parameters like field of view of the camera.
+                    </li>
+                    <li>
+                        The physics. This was the really difficult part. We had to model a number of visual phenomena. For 
+                        example, reflection/refraction through materials, total internal reflection, Fresnel effects, 
+                        Phong local illumination, or shadows. Most of these are very difficult to achieve in scan-conversion 
+                        rendering, while raytracing excels in this area.
+                    </li>
+                </ol>
+            </p>
+            <p>
+                I developed in Visual Studio 15 and the project ended up being basically the largest piece I'd written so far.
+                The class hierarchy and such is probably extremely jank, but I haven't had other eyes on it. I am particularly
+                proud of how I dealt with some odd interactions between classes: it ended up paying off since I had one of the
+                fasted raytracers in the class.
+            </p>
         </div>
     </div>
 </body>
