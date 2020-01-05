4c4e11e9adf5ad9e3095c278c2e683bb827413c1
April Roszkowski <april@rosz.net>
2020-01-05 17:55:55 -0600

    fix(projects): fixes thumbnail overflow issue

--- projects.php
+++ projects.php
@@ -5,38 +5,46 @@
 <link rel='stylesheet' href='/css/site_style.css'></link>
 <link rel='stylesheet' href='/css/header.css'></link>
 <style type='text/css'>
+:root {
+    --entry-height: 200px;
+}
     div.tile {
         border-left: solid coral;
-        height: 200px;
+        height: var(--entry-height);
         margin: 1% var(--column-margin) 1% var(--column-margin);
     }
-    div.tile img {
-        float: left;
-        width: auto;
-        height: auto;
-        max-width: 45%;
-        max-height: 100%;
-    }
-    div.tile div.center {
-        line-height: 1em;
-        float: left;
-        max-width: 47%;
-        max-height: 90%;
-        overflow: hidden;
-    }
-    div.tile .tileLink div {
-        background-color: coral;
-        /* color: coral; */
-        float: right;
-        width: 8%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    div.tile a.tileLink:visited {
-        color: currentColor;    
-    }
+        div.tile div.image-container {
+            float: left;
+            width: calc(var(--entry-height) + 2%);
+            height: 100%;
+            overflow: hidden;
+            position: relative;
+        }
+            div.tile div.image-container img {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%) scale(0.35);
+            }
+        div.tile div.center {
+            line-height: 1em;
+            float: left;
+            width: 47%;
+            overflow: hidden;
+        }
+        div.tile .tileLink div {
+            background-color: coral;
+            /* color: coral; */
+            float: right;
+            width: 8%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+            div.tile a.tileLink:visited {
+                color: currentColor;    
+            }
 </style>
 </head>
 
@@ -47,7 +55,9 @@
 </div>
 
 <div class="tile">
-    <img src="/resources/placeholder.png" alt="a placeholder image">
+    <div class='image-container'>
+        <img src="/resources/raytracer/two_spheres.png" alt="sample output from my ray tracer">
+    </div>
     
     <div class="center">
         <h2>Raytracer</h2>
