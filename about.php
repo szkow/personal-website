dbc5d2463811a98a055b195609df886c6204ebd4
April Roszkowski <april@rosz.net>
2020-01-10 17:37:38 -0600

    fix(about): footer (fixes #5)

--- about.php
+++ about.php
@@ -35,40 +35,42 @@
 <body>
 <?php include 'html/ripple.html' ?>
 <?php include $_SERVER["DOCUMENT_ROOT"] . "/html/header.html" ?>
-
-<div id='page-title'>
-    <h1>About</h1>
-</div>
-
-<div id='center-column'>
-    <p>Welcome to my website!</p>
-    <p>My name is April Roszkowski, a Computer Science student
-        at the University of Minnesota-Twin Cities. This page
-        is most definitely under construction, but for the
-        time being, feel free to view  
-        <a href='/resources/resume.pdf'>my resume</a>.
-    </p>
-</div>
-
-<div id='social-links'>
-    <p>
-    <a href='mailto:roszk008@umn.edu'>
-            <i class="far fa-envelope" style='color: black'></i> roszk008@umn.edu</a><br>
-    <a href='https://www.linkedin.com/in/april-roszkowski'>
-        <i class="fab fa-linkedin" style='color: black'></i> april-roszkowski</a><br>
-    <a href='https://www.github.com/szkow'>
-        <i class="fab fa-github-square" style='color: black'></i> szkow</a><br>
-
-    <a href='https://www.last.fm/user/szkow'>
-        <i class="fab fa-lastfm-square" style='color: black'></i> szkow</a><br>
-    </p>
-</div>
-
-<footer>
-    <div>
-        All content and design by <a href='/about.php'>me</a> &#183 icons 
-        by <a href='https://fontawesome.com'>fontawesome</a>.
+<div id="page-container">
+    
+    <div id="about-container">
+        <div id='page-title'>
+            <h1>About</h1>
+        </div>
+        
+        <div id='center-column'>
+            <p>Welcome to my website!</p>
+            <p>My name is April Roszkowski, a Computer Science student
+                at the University of Minnesota-Twin Cities. This page
+                is most definitely under construction, but for the
+                time being, feel free to view  
+                <a href='/resources/resume.pdf'>my resume</a>.
+            </p>
+        </div>
+        
+        <div id='social-links'>
+            <p>
+            <a href='mailto:roszk008@umn.edu'>
+                    <i class="far fa-envelope" style='color: black'></i> roszk008@umn.edu</a><br>
+            <a href='https://www.linkedin.com/in/april-roszkowski'>
+                <i class="fab fa-linkedin" style='color: black'></i> april-roszkowski</a><br>
+            <a href='https://www.github.com/szkow'>
+                <i class="fab fa-github-square" style='color: black'></i> szkow</a><br>
+        
+            <a href='https://www.last.fm/user/szkow'>
+                <i class="fab fa-lastfm-square" style='color: black'></i> szkow</a><br>
+            </p>
+        </div>
     </div>
-</footer>
+    
+    <footer>
+            All content and design by <a href='/about.php'>me</a> &#183 icons 
+            by <a href='https://fontawesome.com'>fontawesome</a>.
+    </footer>
+</div>
 </body>
 </html>
