8402cf01b1adb1f861a6dbf62f676ce789676927
April Roszkowski <april@rosz.net>
2020-01-15 10:30:54 -0600

    fix(music): fix and improvement to last.fm script (#9)
    - Gets track info by tag, not order in xml
    - Gets more info now
    - Restructures code in lastfm_request.js

--- js/lastfm_request.js
+++ js/lastfm_request.js
@@ -2,33 +2,46 @@ window.onload = (loadEvent) => {
     const kNumTracks = 5;
     var recentScrobbles = document.getElementById('recently-played');
 
-    function printResponse() {
-        console.log(this.responseText);
-    }
-
-    function processResponse() {
-        var response = this.responseXML;
-        var trackList = response.getElementsByTagName('track');
-        for (var i = 0; i < Math.min(trackList.length, kNumTracks); i++) {
-            var trackInfo = trackList.item(i).children;  // Gets children of a given track
-            
-            // Parse data from XML
-            var artist = trackInfo[0].innerHTML;
-            var title = trackInfo[1].innerHTML;
-
-            // Make a new div
-            var newTrack = document.createElement('div');
-            newTrack.innerText = artist + ' - ' + title;
-
-            // Add the div to the DOM
-            recentScrobbles.appendChild(newTrack);
-        }
-    }
-
     var request = new XMLHttpRequest();
     request.addEventListener('load', processResponse);
     request.open('GET',
         'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=szkow&api_key=4e4274667141deb75c3ce0d94ae90938');
     request.responseType = 'document';
     request.send();
+
+    function processResponse() {
+        var response = this.responseXML;
+        var trackList = response.getElementsByTagName('track');
+        for (var i = 0; i < Math.min(trackList.length, kNumTracks); i++) {
+            var track = trackList.item(i);  // Gets reference to a track tag
+            
+            // Parse data from XML
+            // See https://www.last.fm/api/show/user.getRecentTracks
+            var artist = track.getElementsByTagName('artist').item(0).innerHTML;
+            var title = track.getElementsByTagName('name').item(0).innerHTML;
+            var album = track.getElementsByTagName('album').item(0).innerHTML;
+            var date = track.getElementsByTagName('date').item(0).innerHTML;
+
+            // Make a new div
+            var newTrack = makeTrackElement(artist, title, album, date);
+
+            // Add the div to the DOM
+            recentScrobbles.appendChild(newTrack);
+        }
+    }
+
+    function makeTrackElement(artist, title, album, date) {
+        var container = document.createElement('div');
+        var artistElement = document.createElement('h1');
+        artistElement.innerHTML = artist;
+        var titleElement = document.createElement('h2');
+        titleElement.innerHTML = title;
+        var albumElement = document.createElement('h3');
+        albumElement.innerHTML = album;
+        var dateElement = document.createElement('h4');
+        dateElement.innerHTML = date;
+
+        container.append(artistElement, titleElement, albumElement, dateElement);
+        return container;
+    }
 };
