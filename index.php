c0e5fbb6e5cb87e3a6b20fd2c56eb1a757e1d40a
April Roszkowski <april@rosz.net>
2020-01-17 14:14:20 -0600

    fix(index): redirects index to about

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script src="https://kit.fontawesome.com/6dc1dfc54e.js" crossorigin="anonymous"></script>
-<link rel='stylesheet' href='css/site_style.css'></link>
-<link rel='stylesheet' href='css/header.css'></link>
-<link rel='stylesheet' href='css/ripple.css'></link>
-<script src='js/ripple.js'></script>
-</head>
-<body>
-<?php include 'html/ripple.html' ?>
-<?php include 'html/header.html' ?>
-
-<div id='page-title'>
-    <h1>Index</h1>
-</div>
-
-</body>
-</html>
+<?php
+    header('Location: about.php')
+?>
